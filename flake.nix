62d8b5e0ed019067cea2ecb96f30082f368e5f37
David Richey <darichey1@gmail.com>
2022-09-10 02:22:11 -0500

    Set up sqlx with sqlite

--- flake.nix
+++ flake.nix
@@ -1,8 +1,21 @@
 {
-  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
-
-  outputs = inputs:
-    inputs.nci.lib.makeOutputs {
-      root = ./.;
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # Rust dev
+          rustc
+          cargo
+          rust-analyzer
+          rustfmt
+          clippy
+        ];
+      };
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::{StreamExt, TryStreamExt};
+use sqlx::{Connection, SqliteConnection};
+
+#[derive(sqlx::FromRow, Debug)]
+struct Foo {
+    id: i64,
+    name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
+
+    sqlx::query("create table foo ( id int primary key, name string )")
+        .execute(&mut conn)
+        .await?;
+
+    sqlx::query("insert into foo values (1, \"foo\")")
+        .execute(&mut conn)
+        .await?;
+
+    sqlx::query("insert into foo values (2, \"bar\")")
+        .execute(&mut conn)
+        .await?;
+
+    let stream = sqlx::query_as::<_, Foo>("select * from foo").fetch(&mut conn);
+
+    println!("{:?}", stream.try_collect::<Vec<Foo>>().await?);
+
+    Ok(())
 }
