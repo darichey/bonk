4735257a0dc71f1db0cd0a825e2144ed58272ef1
David Richey <darichey1@gmail.com>
2022-11-14 22:32:28 -0600

    init tauri app

--- flake.nix
+++ flake.nix
@@ -1,14 +1,37 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libraries = with pkgs;[
+          # Tauri
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib.out
+          dbus.lib
+          openssl_3.out
+        ];
+
+        packages = with pkgs; [
+          # Tauri
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+
           # Rust dev
           rustc
           cargo
@@ -16,8 +39,25 @@
           rustfmt
           clippy
 
+          # sqlite
           sqlite
+
+          # javascript dev
+          yarn
         ];
-      };
-    };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            let
+              joinLibs = libs: builtins.concatStringsSep ":" (builtins.map (x: "${x}/lib") libs);
+              libs = joinLibs libraries;
+            in
+            ''
+              export LD_LIBRARY_PATH=${libs}:$LD_LIBRARY_PATH
+            '';
+        };
+      });
 }
