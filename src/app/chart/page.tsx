317890bf621c0d3009e9636a074e75d26ebc4aaa
David Richey <darichey1@gmail.com>
2023-05-03 01:27:55 -0500

    Split chart page

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -1,47 +1,12 @@
-"use client";
+"use server";
 
-import { faker } from "@faker-js/faker";
-import "chart.js/auto";
-import { Line } from "react-chartjs-2";
+import Chart from "./chart";
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as const,
-    },
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
-    },
-  },
-};
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
-
-export default function ChartPage() {
+export default async function ChartPage() {
   return (
     <div>
       <div>Chart page</div>
-      <Line data={data} />
+      <Chart />
     </div>
   );
 }
