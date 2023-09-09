7dab7e80aa00097821c1ef92ff5894a8094f0fe4
David Richey <darichey1@gmail.com>
2023-09-08 22:35:35 -0500

    Use anyhow

--- src/db.rs
+++ src/db.rs
@@ -1,36 +1,35 @@
-use std::{fs, num::ParseIntError, path::Path};
+use std::{fs, path::Path};
 
 use sqlite::{Connection, State, Value};
+
+use anyhow::{Context, Result};
 
 pub struct Db {
     con: Connection,
 }
 
 impl Db {
-    pub fn new<P: AsRef<Path>>(path_to_schema: P, path_to_transactions: P) -> Result<Db, String> {
+    pub fn new<P: AsRef<Path>>(path_to_schema: P, path_to_transactions: P) -> Result<Db> {
         // open db connection
-        let con = sqlite::open(":memory:").map_err(|err| err.to_string())?;
+        let con = sqlite::open(":memory:")?;
 
         // create transactions table
-        let query = fs::read_to_string(path_to_schema).map_err(|err| err.to_string())?;
-        con.execute(query).map_err(|err| err.to_string())?;
+        let query = fs::read_to_string(path_to_schema)?;
+        con.execute(query)?;
 
         // insert some transactions
-        con.execute("BEGIN TRANSACTION")
-            .map_err(|err| err.to_string())?;
+        con.execute("BEGIN TRANSACTION")?;
 
-        let mut csv_reader =
-            csv::Reader::from_path(path_to_transactions).map_err(|err| err.to_string())?;
+        let mut csv_reader = csv::Reader::from_path(path_to_transactions)?;
         for result in csv_reader.records() {
-            let row = result.map_err(|err| err.to_string())?;
+            let row = result?;
 
-            let date = row.get(0).ok_or("Date not present")?;
-            let description = row.get(1).ok_or("Description not present")?;
-            let amount = parse_amount(row.get(2).ok_or("Amount not present")?)?;
+            let date = row.get(0).context("Date not present")?;
+            let description = row.get(1).context("Description not present")?;
+            let amount = parse_amount(row.get(2).context("Amount not present")?)?;
 
-            let mut stmt = con
-                .prepare("INSERT INTO transactions VAlUES (:date, :description, :amount)")
-                .map_err(|err| err.to_string())?;
+            let mut stmt =
+                con.prepare("INSERT INTO transactions VAlUES (:date, :description, :amount)")?;
 
             stmt.bind::<&[(_, Value)]>(
                 &[
@@ -38,42 +37,37 @@ impl Db {
                     (":description", description.into()),
                     (":amount", amount.into()),
                 ][..],
-            )
-            .map_err(|err| err.to_string())?;
+            )?;
 
             while let Ok(State::Row) = stmt.next() {}
         }
-        con.execute("COMMIT").map_err(|err| err.to_string())?;
+        con.execute("COMMIT")?;
 
         Ok(Db { con })
     }
 
-    pub fn get_transactions(&self) -> Result<Vec<(String, String, i64)>, String> {
+    pub fn get_transactions(&self) -> Result<Vec<(String, String, i64)>> {
         let query = "
             SELECT * FROM transactions;
         ";
-        let mut stmt = self.con.prepare(query).map_err(|err| err.to_string())?;
+        let mut stmt = self.con.prepare(query)?;
         let mut vec = Vec::new();
         while let Ok(sqlite::State::Row) = stmt.next() {
             vec.push((
-                stmt.read::<String, _>("date")
-                    .map_err(|err| err.to_string())?,
-                stmt.read::<String, _>("description")
-                    .map_err(|err| err.to_string())?,
-                stmt.read::<i64, _>("amount")
-                    .map_err(|err| err.to_string())?,
+                stmt.read::<String, _>("date")?,
+                stmt.read::<String, _>("description")?,
+                stmt.read::<i64, _>("amount")?,
             ))
         }
         Ok(vec)
     }
 }
 
-fn parse_amount(s: &str) -> Result<i64, String> {
+fn parse_amount(s: &str) -> Result<i64> {
     let negative = s.starts_with('-');
-    let (dollars, cents) = s.split_once('.').ok_or("Couldn't split")?;
-    let dollars: i64 =
-        str::parse(&dollars.replace('-', "")).map_err(|err: ParseIntError| err.to_string())?;
-    let cents: i64 = str::parse(cents).map_err(|err: ParseIntError| err.to_string())?;
+    let (dollars, cents) = s.split_once('.').context("Couldn't split")?;
+    let dollars: i64 = str::parse(&dollars.replace('-', ""))?;
+    let cents: i64 = str::parse(cents)?;
     let amt = dollars * 100 + cents;
     if negative {
         Ok(-amt)
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 
 mod db;
 
-fn main() -> Result<(), String> {
+use anyhow::Result;
+
+fn main() -> Result<()> {
     let db = db::Db::new("./db/schema.sql", "./data/transactions.csv")?;
     for (date, description, amount) in db.get_transactions()? {
         println!("{},{},{}", date, description, amount);
