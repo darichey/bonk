b8bb17ae5c6778e24826fa375fe4cbde86bdaa47
David Richey <darichey1@gmail.com>
2023-09-22 02:23:53 -0500

    Add total assets over time chart

---
+++ src/app/TransactionLog.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useGetAllTransactions } from "./commands";
+
+export default function TransactionLog() {
+  const { data: transactions, error, isLoading } = useGetAllTransactions();
+
+  return (
+    <div>
+      <h1 className="font-bold">Your Transaction Log</h1>
+      {isLoading ? (
+        <div>transactions here... loading...</div>
+      ) : error ? (
+        <div>Encountered error: {error}</div>
+      ) : (
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Account</th>
+            <th>Amount</th>
+          </tr>
+          {transactions?.map((transaction, i) => (
+            <tr key={i}>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.account}</td>
+              <td>{transaction.amount.cents}</td>
+            </tr>
+          ))}
+        </table>
+      )}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,13 @@
 "use client";
 
-import { useGetAllTransactions } from "./commands";
+import TotalAssetsOverTime from "./TotalAssetsOverTime";
+import TransactionLog from "./TransactionLog";
 
 export default function Home() {
-  const { data: transactions, error, isLoading } = useGetAllTransactions();
-
   return (
-    <div>
-      <h1 className="font-bold">Your Transaction Log</h1>
-      {isLoading ? (
-        <div>transactions here... loading...</div>
-      ) : error ? (
-        <div>Encountered error: {error}</div>
-      ) : (
-        <table>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Account</th>
-            <th>Amount</th>
-          </tr>
-          {transactions?.map((transaction, i) => (
-            <tr key={i}>
-              <td>{transaction.date}</td>
-              <td>{transaction.description}</td>
-              <td>{transaction.account}</td>
-              <td>{transaction.amount.cents}</td>
-            </tr>
-          ))}
-        </table>
-      )}
-    </div>
+    <main>
+      {/* <TransactionLog /> */}
+      <TotalAssetsOverTime />
+    </main>
   );
 }
