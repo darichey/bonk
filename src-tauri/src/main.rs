8615411a746af89560d93b61ce62bad2a002341f
David Richey <darichey1@gmail.com>
2023-10-31 23:51:29 -0500

    Remove unused rust features

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![feature(iterator_try_collect)]
-#![feature(try_find)]
 
 use std::sync::Mutex;
 
