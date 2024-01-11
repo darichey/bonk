8ee3633da2faa5193b1f2b55684f6c6c969beb1f
David Richey <darichey1@gmail.com>
2024-01-11 00:24:58 -0600

    Set up lsp client

--- src/vscode-extension/src/extension.ts
+++ src/vscode-extension/src/extension.ts
@@ -1,9 +1,56 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("bonk.helloWorld", () => {
-    vscode.window.showInformationMessage("Hello World from bonk!");
-  });
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
-  context.subscriptions.push(disposable);
+let client: LanguageClient | undefined;
+
+export function activate(_context: vscode.ExtensionContext) {
+  const output = vscode.window.createOutputChannel("Bonk");
+  const traceOutputChannel =
+    vscode.window.createOutputChannel("Bonk LSP Trace");
+
+  const serverCommand = process.env["__BONK_LSP_SERVER_DEBUG"];
+  if (!serverCommand) {
+    output.appendLine(`No path to lsp binary, not activating`);
+    return;
+  } else {
+    output.appendLine(`Using LSP binary: ${serverCommand}`);
+  }
+
+  const serverOptions: ServerOptions = {
+    run: {
+      command: serverCommand,
+      transport: TransportKind.stdio,
+    },
+    debug: {
+      command: serverCommand,
+      transport: TransportKind.stdio,
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "bonk" }],
+    traceOutputChannel,
+  };
+
+  client = new LanguageClient(
+    "bonk",
+    "Bonk Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
