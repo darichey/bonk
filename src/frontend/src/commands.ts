2139017f813e900342e7f228e666e4370a9cda60
David Richey <darichey1@gmail.com>
2024-01-29 23:32:54 -0600

    Add stub implementations for frontend fetching

--- src/frontend/src/commands.ts
+++ src/frontend/src/commands.ts
@@ -2,30 +2,36 @@ import useSWR, { SWRResponse } from "swr";
 import { ChartData, Dashboard, TableData, Transaction } from "./types";
 
 export function useGetAllTransactions(): SWRResponse<Transaction[]> {
-  throw new Error("unimplemented"); // TODO
+  return useSWR("useGetAllTransactions", () => [] as Transaction[]); // TODO
 }
 
 export function useQueryTransactionsForChart(
   query: string
 ): SWRResponse<ChartData> {
-  throw new Error("unimplemented"); // TODO
+  return useSWR("useQueryTransactionsForChart", () => ({} as ChartData)); // TODO
 }
 
 export function useQueryTransactions(query: string): SWRResponse<TableData> {
-  throw new Error("unimplemented"); // TODO
+  return useSWR(
+    "useQueryTransactions",
+    () => ({ column_names: [], data: [] } as TableData)
+  ); // TODO
 }
 
 export function useGetDashboardNames(): SWRResponse<string[]> {
-  throw new Error("unimplemented"); // TODO
+  return useSWR("useGetDashboardNames", () => [] as string[]); // TODO
 }
 
 export function useGetDashboard(name: string): SWRResponse<Dashboard> {
-  throw new Error("unimplemented"); // TODO
+  return useSWR(
+    "useGetDashboard",
+    () => ({ name: "TODO", components: [] } as Dashboard)
+  ); // TODO
 }
 
 export function useRenderQueryTemplate(
   template: string,
   variables: Record<string, string>
 ): SWRResponse<string> {
-  throw new Error("unimplemented"); // TODO
+  return useSWR("useRenderQueryTemplate", () => "TODO"); // TODO
 }
