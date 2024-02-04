71606eb18c9c7d42c343a5d2f3702199dc33464a
David Richey <darichey1@gmail.com>
2024-02-03 19:51:04 -0600

    Remove tauri stuff from flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
   };
@@ -12,16 +12,6 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        libraries = with pkgs;[
-          webkitgtk
-          gtk3
-          cairo
-          gdk-pixbuf
-          glib
-          dbus
-          openssl_3
-          librsvg
-        ];
       in
       with pkgs;
       {
@@ -32,6 +22,8 @@
               extensions = [ "rust-src" ];
             })
 
+            pkg-config
+            openssl_3
             tree-sitter
 
             # Generate rust plaid client
@@ -40,62 +32,12 @@
             # JS dev
             nodejs_20
 
-            # Tauri deps
-            curl
-            wget
-            pkg-config
-            dbus
-            openssl_3
-            glib
-            gtk3
-            libsoup
-            webkitgtk
-            librsvg
-            glib-networking
-
             # Python dev
             (python3.withPackages (ps: with ps; [ pandas pytorch ]))
 
             # just for debugging
             sqlite
             rlwrap
-          ];
-
-          shellHook =
-            ''
-              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
-              export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/";
-            '';
-        };
-
-        packages.default = rustPlatform.buildRustPackage rec {
-          pname = "finance-app";
-          version = "0.1.0";
-
-          src = builtins.path {
-            path = ./.;
-            name = "finance-app";
-          };
-
-          sourceRoot = "finance-app/src-tauri";
-
-          cargoLock = {
-            lockFile = ./src-tauri/Cargo.lock;
-          };
-
-          nativeBuildInputs = [
-            copyDesktopItems
-            wrapGAppsHook
-            pkg-config
-          ];
-
-          buildInputs = [
-            openssl
-            dbus
-            glib
-            glib-networking
-            libayatana-appindicator
-            webkitgtk
           ];
         };
       }
