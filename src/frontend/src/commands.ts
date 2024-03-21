2bdaa85dc56a7f874f8f69b2cd2ec12a1770f49c
David Richey <darichey1@gmail.com>
2024-03-20 23:37:32 -0500

    frontend: wire up /dashboard

--- src/frontend/src/commands.ts
+++ src/frontend/src/commands.ts
@@ -36,10 +36,11 @@ export function useGetDashboardNames(): SWRResponse<string[]> {
 }
 
 export function useGetDashboard(name: string): SWRResponse<Dashboard> {
-  return useSWR(
-    "useGetDashboard",
-    () => ({ name: "TODO", components: [] } as Dashboard)
-  ); // TODO
+  return useSWR(["/dashboard", name], async ([_, name]) => {
+    const res = await fetch(`http://localhost:8080/dashboard`);
+    const json = await res.json();
+    return json as Dashboard;
+  });
 }
 
 export function useRenderQueryTemplate(
