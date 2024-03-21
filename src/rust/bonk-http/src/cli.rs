3c4cfb3bc2c7f1d32461ff5bdbedd46f743eb21b
David Richey <darichey1@gmail.com>
2024-03-20 22:56:22 -0500

    bonk-http: move main server code to lib

--- src/rust/bonk-http/src/cli.rs
+++ src/rust/bonk-http/src/cli.rs
@@ -1,17 +1,6 @@
-use std::{
-    path::PathBuf,
-    sync::{Arc, Mutex},
-};
+use std::path::PathBuf;
 
-use anyhow::Result;
-use bonk_check::WorkspaceExt as _;
-use bonk_db::Db;
-use bonk_parse::WorkspaceExt as _;
-use bonk_workspace::Workspace;
 use clap::Parser;
-use rouille::{router, Response, Server};
-
-use crate::{get_transactions::get_transactions, query_transactions::query_transactions, State};
 
 /// Starts an http server that can be used to interact with the Bonk workspace
 #[derive(Parser, Debug)]
@@ -22,44 +11,6 @@ pub struct Args {
     pub cfg: PathBuf,
 }
 
-pub fn run(args: Args) -> Result<()> {
-    let state = {
-        let Args { cfg } = args;
-
-        let workspace = Workspace::from_cfg(cfg).expect("Couldn't read cfg");
-        let workspace = workspace.parse().unwrap();
-        let workspace = workspace.check().unwrap();
-
-        let state = State {
-            db: Db::new(&workspace, ":memory:").expect("Couldn't create database"),
-        };
-
-        Arc::new(Mutex::new(state))
-    };
-
-    let server = Server::new("localhost:8080", move |request| {
-        if request.method() == "OPTIONS" {
-            // TOOD: real cors
-            return Response::empty_204()
-                .with_additional_header("Access-Control-Allow-Origin", "*")
-                .with_additional_header("Access-Control-Allow-Methods", "*")
-                .with_additional_header("Access-Control-Allow-Headers", "*");
-        }
-
-        let response = router!(request,
-            (GET) (/transactions) => { get_transactions(request, state.clone()) },
-            (POST) (/queryTransactions) => { query_transactions(request, state.clone()) },
-            _ => Response::empty_404(),
-        );
-
-        // TODO: real cors
-        response.with_additional_header("Access-Control-Allow-Origin", "*")
-    })
-    .unwrap();
-
-    println!("Listening on port {}", server.server_addr().port());
-
-    server.run();
-
-    Ok(())
+pub fn run(args: Args) -> anyhow::Result<()> {
+    crate::run(&args.cfg)
 }
--- src/rust/bonk-http/src/lib.rs
+++ src/rust/bonk-http/src/lib.rs
@@ -2,9 +2,59 @@ pub mod cli;
 mod get_transactions;
 mod query_transactions;
 
-use bonk_db::Db;
+use std::{
+    path::Path,
+    sync::{Arc, Mutex},
+};
 
+use bonk_check::WorkspaceExt as _;
+use bonk_db::Db;
+use bonk_parse::WorkspaceExt as _;
+use bonk_workspace::Workspace;
+use rouille::{router, Response, Server};
 use serde::Serialize;
+
+use crate::{get_transactions::get_transactions, query_transactions::query_transactions};
+
+pub fn run(cfg: &Path) -> anyhow::Result<()> {
+    let state = {
+        let workspace = Workspace::from_cfg(cfg).expect("Couldn't read cfg");
+        let workspace = workspace.parse().unwrap();
+        let workspace = workspace.check().unwrap();
+
+        let state = State {
+            db: Db::new(&workspace, ":memory:").expect("Couldn't create database"),
+        };
+
+        Arc::new(Mutex::new(state))
+    };
+
+    let server = Server::new("localhost:8080", move |request| {
+        if request.method() == "OPTIONS" {
+            // TOOD: real cors
+            return Response::empty_204()
+                .with_additional_header("Access-Control-Allow-Origin", "*")
+                .with_additional_header("Access-Control-Allow-Methods", "*")
+                .with_additional_header("Access-Control-Allow-Headers", "*");
+        }
+
+        let response = router!(request,
+            (GET) (/transactions) => { get_transactions(request, state.clone()) },
+            (POST) (/queryTransactions) => { query_transactions(request, state.clone()) },
+            _ => Response::empty_404(),
+        );
+
+        // TODO: real cors
+        response.with_additional_header("Access-Control-Allow-Origin", "*")
+    })
+    .unwrap();
+
+    println!("Listening on port {}", server.server_addr().port());
+
+    server.run();
+
+    Ok(())
+}
 
 struct State {
     db: Db,
