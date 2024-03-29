e5fe8ba9c331d0a00417f80e89ae7a24fdb745ee
David Richey <darichey1@gmail.com>
2024-03-29 16:46:24 -0500

    bonk-lsp: Move init code to lib.rs

--- src/rust/bonk-lsp/src/cli.rs
+++ src/rust/bonk-lsp/src/cli.rs
@@ -1,17 +1,8 @@
-use bonk_workspace::Workspace;
 use clap::Parser;
-use lsp_types::ServerCapabilities;
-use lsp_types::{
-    CompletionOptions, DiagnosticOptions, DiagnosticServerCapabilities, OneOf,
-    TextDocumentSyncCapability, TextDocumentSyncKind, TextDocumentSyncOptions,
-    WorkDoneProgressOptions,
-};
 
 use std::path::PathBuf;
 
-use lsp_server::Connection;
-
-use crate::main_loop;
+use crate::run_server;
 
 /// Starts a Bonk LSP server
 #[derive(Parser)]
@@ -26,54 +17,6 @@ pub struct Args {
 
 pub fn run(args: Args) -> anyhow::Result<()> {
     let Args { cfg_path, .. } = args;
-
-    let workspace = Workspace::from_cfg(cfg_path).expect("failed to parse workspace");
-
-    eprintln!("Starting...");
-
-    // Create the transport. Includes the stdio (stdin and stdout) versions but this could
-    // also be implemented to use sockets or HTTP.
-    let (connection, io_threads) = Connection::stdio();
-
-    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
-    let server_capabilities = serde_json::to_value(ServerCapabilities {
-        text_document_sync: Some(TextDocumentSyncCapability::Options(
-            TextDocumentSyncOptions {
-                open_close: Some(true),
-                change: Some(TextDocumentSyncKind::INCREMENTAL),
-                will_save: Some(false),
-                will_save_wait_until: Some(false),
-                save: None,
-            },
-        )),
-        diagnostic_provider: Some(DiagnosticServerCapabilities::Options(DiagnosticOptions {
-            identifier: Some("bonk".to_string()),
-            inter_file_dependencies: true,
-            workspace_diagnostics: true, // FIXME: we should respond to workspace/diagnostic requests
-            work_done_progress_options: WorkDoneProgressOptions {
-                work_done_progress: Some(false),
-            },
-        })),
-        completion_provider: Some(CompletionOptions {
-            ..Default::default()
-        }),
-        definition_provider: Some(OneOf::Left(true)),
-        ..Default::default()
-    })
-    .unwrap();
-    let initialization_params = match connection.initialize(server_capabilities) {
-        Ok(it) => it,
-        Err(e) => {
-            if e.channel_is_disconnected() {
-                io_threads.join()?;
-            }
-            return Err(e.into());
-        }
-    };
-    main_loop(connection, initialization_params, workspace)?;
-    io_threads.join()?;
-
-    // Shut down gracefully.
-    eprintln!("Shutting down...");
+    run_server(cfg_path)?;
     Ok(())
 }
--- src/rust/bonk-lsp/src/lib.rs
+++ src/rust/bonk-lsp/src/lib.rs
@@ -5,21 +5,78 @@ mod go_to_def;
 mod state;
 mod util;
 
+use std::path::PathBuf;
+
 use bonk_parse::WorkspaceExt;
 use bonk_workspace::Workspace;
 use lsp_server::{Connection, ExtractError, Message, Notification, Request, RequestId, Response};
 use lsp_types::notification::{DidChangeTextDocument, DidOpenTextDocument};
 use lsp_types::request::{Completion, DocumentDiagnosticRequest, GotoDefinition};
-use lsp_types::InitializeParams;
 use lsp_types::{
-    CompletionList, DocumentDiagnosticReport, FullDocumentDiagnosticReport, GotoDefinitionResponse,
-    RelatedFullDocumentDiagnosticReport,
+    CompletionList, CompletionOptions, DiagnosticOptions, DiagnosticServerCapabilities,
+    DocumentDiagnosticReport, FullDocumentDiagnosticReport, GotoDefinitionResponse,
+    RelatedFullDocumentDiagnosticReport, ServerCapabilities, TextDocumentSyncCapability,
+    TextDocumentSyncKind, TextDocumentSyncOptions, WorkDoneProgressOptions,
 };
+use lsp_types::{InitializeParams, OneOf};
 
 use crate::complete::get_completion_results;
 use crate::diagnostic::get_doc_diagnostics;
 use crate::go_to_def::get_go_to_def_result;
 use crate::state::State;
+
+fn run_server(cfg_path: PathBuf) -> anyhow::Result<()> {
+    let workspace = Workspace::from_cfg(cfg_path).expect("failed to parse workspace");
+
+    eprintln!("Starting...");
+
+    // Create the transport. Includes the stdio (stdin and stdout) versions but this could
+    // also be implemented to use sockets or HTTP.
+    let (connection, io_threads) = Connection::stdio();
+
+    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
+    let server_capabilities = serde_json::to_value(ServerCapabilities {
+        text_document_sync: Some(TextDocumentSyncCapability::Options(
+            TextDocumentSyncOptions {
+                open_close: Some(true),
+                change: Some(TextDocumentSyncKind::INCREMENTAL),
+                will_save: Some(false),
+                will_save_wait_until: Some(false),
+                save: None,
+            },
+        )),
+        diagnostic_provider: Some(DiagnosticServerCapabilities::Options(DiagnosticOptions {
+            identifier: Some("bonk".to_string()),
+            inter_file_dependencies: true,
+            workspace_diagnostics: true, // FIXME: we should respond to workspace/diagnostic requests
+            work_done_progress_options: WorkDoneProgressOptions {
+                work_done_progress: Some(false),
+            },
+        })),
+        completion_provider: Some(CompletionOptions {
+            ..Default::default()
+        }),
+        definition_provider: Some(OneOf::Left(true)),
+        ..Default::default()
+    })
+    .unwrap();
+    let initialization_params = match connection.initialize(server_capabilities) {
+        Ok(it) => it,
+        Err(e) => {
+            if e.channel_is_disconnected() {
+                io_threads.join()?;
+            }
+            return Err(e.into());
+        }
+    };
+    main_loop(connection, initialization_params, workspace)?;
+    io_threads.join()?;
+
+    // Shut down gracefully.
+    eprintln!("Shutting down...");
+
+    Ok(())
+}
 
 fn main_loop(
     connection: Connection,
